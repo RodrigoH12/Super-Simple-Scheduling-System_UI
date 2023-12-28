181713bdc8a0d888e2e2820f23d3d29c38db4280
Rodrigo Heredia <rodrigoh1205@gmail.com>
2023-12-28 12:40:46 -0400

    [Task-7] Created new component user-info-card

---
+++ src/app/pages/home-page/components/userSection/user-info-card/user-info-card.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { State, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { getStudentClasses } from 'src/app/core/store/actions/student.actions';
+import { initialState } from 'src/app/core/store/reducers/student.reducer';
+import { selectStudent } from 'src/app/core/store/selectors/student.selector';
+import { Student } from 'src/app/shared/models/student.model';
+
+@Component({
+    selector: 'app-user-info-card',
+    templateUrl: './user-info-card.component.html',
+    styleUrls: ['./user-info-card.component.scss'],
+})
+export class UserInfoCardComponent {
+    student$: Observable<Student> = this.store.select(selectStudent);
+    student: Student = {
+        id: '',
+        firstName: 'Loading',
+        lastName: 'User',
+        userId: '',
+        user: null,
+        classes: [],
+    };
+
+    constructor(private store: Store<State<Student>>) {}
+
+    ngOnInit(): void {
+        this.student$.subscribe((student) => {
+            if (student != initialState.student) {
+                this.student = student;
+            }
+        });
+        this.store.dispatch(
+            getStudentClasses({
+                studentId: 'f5d72e55-a870-4f1a-9e80-08db4d6c094c',
+            })
+        );
+    }
+}
