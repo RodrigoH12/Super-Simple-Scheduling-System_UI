098bf9546f563fe77c0bfba3b5d23d105f12e4fd
Rodrigo Heredia <rodrigoh1205@gmail.com>
2023-12-27 20:38:17 -0400

    [Task-4] Implemented ngrx store action, effect, reducer and selector for Users

--- src/app/core/store/state/app.state.ts
+++ src/app/core/store/state/app.state.ts
@@ -7,25 +7,29 @@ import { EffectsModule } from '@ngrx/effects';
 import { NgModule } from '@angular/core';
 import { classReducer, ClassesState } from '../reducers/class.reducer';
 import { studentReducer, StudentsState } from '../reducers/student.reducer';
+import { userReducer, UsersState } from '../reducers/user.reducer';
 import { ClassEffects } from '../effects/class.effects';
 import { StudentEffects } from '../effects/student.effects';
+import { UserEffects } from '../effects/user.effects';
 
 export const FEATURE_KEY = 'store';
 
 export interface AppState {
     class: ClassesState;
     student: StudentsState;
+    user: UsersState;
 }
 
 export const reducers: ActionReducerMap<any> = {
     class: classReducer,
     student: studentReducer,
+    user: userReducer,
 };
 
 @NgModule({
     imports: [
         StoreModule.forFeature(FEATURE_KEY, reducers),
-        EffectsModule.forFeature([ClassEffects, StudentEffects]),
+        EffectsModule.forFeature([ClassEffects, StudentEffects, UserEffects]),
     ],
 })
 export class SchedulingStateModule {}
