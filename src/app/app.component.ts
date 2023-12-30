75c037a708923085792a388e374235ea4eaee130
Rodrigo Heredia <rodrigoh1205@gmail.com>
2023-12-30 02:22:37 -0400

    [Task-9] Removed redundant code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { State, Store } from '@ngrx/store';
-import { Class } from './shared/models/class.model';
 import { NavigationStart, Router } from '@angular/router';
 
 @Component({
@@ -13,7 +11,7 @@ export class AppComponent {
 
     loginPage: boolean = false;
 
-    constructor(private router: Router, private store: Store<State<Class>>) {
+    constructor(private router: Router) {
         router.events.forEach((event) => {
             if (event instanceof NavigationStart) {
                 if (event['url'] == '/login' || event['url'] == '/') {
