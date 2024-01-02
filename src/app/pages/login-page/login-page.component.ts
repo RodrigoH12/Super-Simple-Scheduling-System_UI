8ea638064f9a7f805cacd2f80024a250210cfb02
Rodrigo Heredia <rodrigoh1205@gmail.com>
2024-01-02 07:35:01 -0400

    [Task-10] Implemented failed login exception in Login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { State, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { login } from 'src/app/core/store/actions/login.actions';
+import { login, loginFail } from 'src/app/core/store/actions/login.actions';
 import { initialState } from 'src/app/core/store/reducers/login.reducer';
-import { selectLoggedUser } from 'src/app/core/store/selectors/login.selector';
+import {
+    selectErrorMessage,
+    selectLoggedUser,
+} from 'src/app/core/store/selectors/login.selector';
 import { User } from 'src/app/shared/models/user.model';
 import { getStudentClasses } from 'src/app/core/store/actions/student.actions';
 
@@ -18,6 +21,10 @@ export class LoginPageComponent {
     password: string = '';
 
     login$: Observable<User> = this.store.select(selectLoggedUser);
+    loginError$: Observable<string> = this.store.select(selectErrorMessage);
+    loginError: string = '';
+
+    @ViewChild('errorToast') errorToast: any;
 
     constructor(private router: Router, private store: Store<State<User>>) {}
 
@@ -27,15 +34,38 @@ export class LoginPageComponent {
                 this.store.dispatch(
                     getStudentClasses({ studentId: user.student!.id })
                 );
+                this.cleanLoginFail();
                 this.router.navigate(['/home']);
             }
         });
     }
 
+    ngAfterViewInit(): void {
+        this.loginError$.subscribe((error) => {
+            if (error != initialState.errorMessage) {
+                this.showToast(error);
+            }
+        });
+    }
+
     login(): void {
-        this.store.dispatch(
-            login({ username: this.username, password: this.password })
-        );
+        if (this.username && this.password) {
+            this.store.dispatch(
+                login({ username: this.username, password: this.password })
+            );
+        }
+    }
+
+    showToast(message: string): void {
+        this.loginError = message;
+    }
+
+    hideToast(): void {
+        this.loginError = '';
+        this.cleanLoginFail();
+    }
+
+    cleanLoginFail(): void {
+        this.store.dispatch(loginFail({ errorMsg: '' }));
     }
 }
-
